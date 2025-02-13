bb810464509ec7ccfce452a9994cbc972b5af4b9
jaykhandla <khandlajay264@gmail.com>
2025-02-13 14:45:08 +0530

    files added

---
+++ script.js
@@ -0,0 +1,32 @@
+/* mouse bubble effect */
+function spark(event) {
+    let i = document.createElement('i');
+    i.style.left = (event.pageX) + 'px';
+    i.style.top = (event.pageY) + 'px';
+    i.style.scale = `${Math.random() * 2 + 1}`;
+    i.style.setProperty('--x', getRandomTransitionValue());
+    i.style.setProperty('--y', getRandomTransitionValue());
+
+    document.body.appendChild(i);
+
+    setTimeout(() => {
+        document.body.removeChild(i);
+    }, 2000);
+};
+
+function getRandomTransitionValue() {
+    return `${Math.random() * 400 - 200}px`;
+};
+
+document.addEventListener('mousemove', spark);
+
+
+/* you cant copy */
+document.addEventListener("copy", (event) => {
+    const selectedData = window.getSelection().toString();
+    event.clipboardData.setData(
+        "text/plain",
+        "♠"
+    );
+    event.preventDefault();
+});
